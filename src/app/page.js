400fb9b4d19f55843a018b15c51c098b10f4ce4b
karenreina15 <karenlisbethro@ufps.edu.co>
2026-04-15 15:02:11 -0500

    feat: actividad NASA APOD
    aplicación que conecta con la API de la NASA

--- src/app/page.js
+++ src/app/page.js
@@ -1,65 +1,148 @@
+"use client";
+import { useEffect, useState } from "react";
+import { RadioButton } from "@/components";
 import Image from "next/image";
 
+
 export default function Home() {
+  /***Define la categoría de búsqueda (Hoy, Fecha o Aleatorio). */
+  const [modo, setModo] = useState("");
+  const [fecha, setFecha] = useState("");
+  const [aleatorio, setAleatorio] = useState("");
+  /***Almacena la respuesta JSON de la API de la NASA. */
+  const [datos, setDato] = useState(null);
+  /***Actúa como interruptor para disparar la consulta al hacer clic. */
+  const [enviar, setEnviar] = useState(false);
+  /***visualización del mensaje de espera */
+  const [cargando, setCargando] = useState(false);
+
+  useEffect(() => {
+    if (enviar) {
+      const consultarApi = async () => {
+        setCargando(true); /***muestra el mensaje de espera mientras la API trae la informacion  */
+        /***direccion de la NASA  */
+        const apiKey = "QPKPNROmnWVJ3aZmy2kFO3Bx56Vff5BSespUrwbr"
+        let url = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`
+        /***depende del modo se realiza la consulta en la API */
+        if (modo === "fecha") {
+          url = `${url}&date=${fecha}`
+        }
+        if (modo === "aleatorio") {
+          url = `${url}&count=${aleatorio}`
+        }
+        /***conecta la aplicación con el servidor de la NASA para solicitar los datos y espera a que los traiga. */
+        const res = await fetch(url);
+        /***Transforma la respuesta que envía la NASA en un objeto de JavaScript */
+        const datos = await res.json();
+        /***depende de si me estan enviando un paquete o un objeto, si es objeto lo guardo en un arreglo */
+        if (Array.isArray(datos)) {
+          setDato(datos)
+        } else {
+          setDato([datos])
+        }
+        setEnviar(false); /***Apago el botón para que no se quede hundido pidiendo cosas infinitamente. */
+        setCargando(false); /***Apago el circulito de espera */
+
+      }
+      consultarApi();
+
+    }
+
+  }, [enviar])
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="mx-8 my-5 py-4 px-4 bg-white font-sans  border-2 rounded-2xl border-gray-300">
+      <h1 className="font-bold">Ejercicio React: NASA APOD : KAREN REINA</h1>
+      <div className="  mx-6 my-5 py-4 px-4 bg-zinc-50 font-sans  border-2 rounded-xl border-gray-300  space-y-4 mt-4">
+        <h2 className="font-bold">1. Modo de consulta </h2>
+        <ul className="flex gap-6">
+          <li>
+            {/* permite elegir entre las 3 opciones de modo  */}  
+            <RadioButton
+              name="modo"
+              value="hoy"
+              label="Foto de hoy"
+              checked={modo === "hoy"}
+              onChange={(e) => setModo(e.target.value)}
+            ></RadioButton>
+          </li>
+
+          <li>
+            <RadioButton
+              name="modo"
+              value="fecha"
+              label="Fecha especifica"
+              checked={modo === "fecha"}
+              onChange={(e) => setModo(e.target.value)}
+            ></RadioButton>
+          </li>
+
+          <li>
+            <RadioButton
+              name="modo"
+              value="aleatorio"
+              label="Aleatorios"
+              checked={modo === "aleatorio"}
+              onChange={(e) => setModo(e.target.value)}
+            ></RadioButton>
+          </li>
+        </ul>
+        {/*evalua que el modo sea igual a la fecha*/} 
+        {modo === "fecha" && (     
+          <div className="flex gap-9 col items-center py-2 my-2 ">
+            <h3 className="text-gray-700 ">Fecha</h3>
+        {/*  Campo de entrada tipo calendario. onChange: Captura el valor seleccionado por el usuario (e.target.value) y lo guarda inmediatamente en el estado setFecha. */}  
+            <input type="date" className="border p-2 rounded-2xl  border-gray-400 text-gray-700 " onChange={(e) => setFecha(e.target.value)}></input>
+          </div>
+        )}
+         {/*evalua que el modo sea igual a aleatorio*/} 
+        {modo === "aleatorio" && (
+          <div className="flex gap-9 col items-center py-2 my-2 ">
+            <h3 className="text-gray-700 ">Numero</h3>
+             {/*  Campo de entrada tipo numerico donde max y min es 10 1. onChange: Captura el valor seleccionado por el usuario (e.target.value) y lo guarda inmediatamente en el estado setaleatorio */}  
+            <input type="number" min="1" max="10" className="border p-2 rounded-2xl  border-gray-400 text-gray-700 " onChange={(e) => setAleatorio(e.target.value)}></input>
+          </div>
+        )}
+         {/*botones*/} 
+        <div className=" mb-2">
+          <button className="bg-blue-500 hover:bg-blue-600 text-white mx-2 my-8 py-2 px-4 rounded-2xl text-xs " id="buscar"
+            onClick={() => {
+              setEnviar(true);
+            }}
+          >Consultar NASA APOD</button>
+          <button className="bg-gray-300 hover:bg-gray-400 mx-2 my-8 py-2 px-4 rounded-2xl text-xs" id="limpiar"
+            onClick={() => {
+              setDato("");
+              setEnviar(false);
+
+            }}>Limpiar resultado</button>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+          {/**Verifica si hay datos. uso .map para recorrer la lista  */}
+        {datos && datos.map((item, index) => (
+          <div key={index} className="flex flex-col items-center ">
+            <h3 className="font-bold text-center">{item.title}</h3>
+            <p className="text-sm text-gray-500">{item.date}</p>
+           {/*Si el recurso es un video, se crea un enlace en una pestaña nueva  */}
+            {item.media_type === "video" && (
+              <a href={item.url} target="_blank" className="text-blue-500 underline mb-4 block">
+                Abrir video en una nueva pestaña
+              </a>
+            )}
+            {/*depende del recurso obtenido si es video o imagen  */}
+            {item.media_type === "video" ? (
+              <div src={item.url} className="my-1"> </div>
+            ) : (
+              <img src={item.url} alt={item.title} width="50%" />
+            )}
+            <p className="mt-4 text-justify">
+              {item.explanation}
+            </p>
+          </div>
+        ))}
+        {cargando && (
+            <p className="mt-4 text-blue-600 font-medium">Conectando con la NASA...</p>
+          
+        )}
+      </div>
     </div>
   );
 }
